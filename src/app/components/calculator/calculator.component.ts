889d6dd0f2dcd856dd1cf15103a1cd4044cb1bf3
DESKTOP-1CHO5AI\David <david07tijola@gmail.com>
2024-02-07 11:05:04 +0100

    create calculator

---
+++ src/app/components/calculator/calculator.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  standalone: true,
+  imports: [],
+  templateUrl: './calculator.component.html',
+  styleUrl: './calculator.component.css'
+})
+export class CalculatorComponent {
+  value: string = '0'
+
+  constructor() {
+  }
+
+  calculate(percentage: boolean) {
+    if (percentage) {
+      let data = this.value.split(/[+\-*\/]/)
+      this.value = this.value.replace(/[+\-*\/].*/, function(match) {
+        let percentageCalculate = (Number(data[1]) / 100) * Number(data[0])
+        return match[0] + percentageCalculate.toString()
+      })
+      this.value = eval(this.value)
+    }
+    else
+      this.value = eval(this.value)
+  }
+
+  addValue(value: any) {
+    if (this.value === '0') {
+      this.value = value.toString()
+    } else {
+      this.value = this.value + value.toString()
+    }
+
+  }
+
+  cleanValue() {
+    this.value = '0'
+  }
+
+  deleteLastValue() {
+    if (this.value.length <= 1)
+      this.value = '0'
+    else
+      this.value = this.value.slice(0, -1)
+  }
+}
